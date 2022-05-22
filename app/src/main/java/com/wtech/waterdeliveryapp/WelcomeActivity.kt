84f7670fe6b87d48eb84d66cd6d6761013592c96
wanjala stephen <stephenwanjala145@gmail.com>
2022-05-22 14:46:04 +0300

    add fb connect

--- app/src/main/java/com/wtech/waterdeliveryapp/WelcomeActivity.kt
+++ app/src/main/java/com/wtech/waterdeliveryapp/WelcomeActivity.kt
@@ -3,6 +3,8 @@ package com.wtech.waterdeliveryapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class WelcomeActivity : AppCompatActivity() {
     private lateinit var signupButton: Button
@@ -10,7 +12,11 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
 
+        myRef.setValue("Hello, john")
 
         signupButton=findViewById(R.id.btnSignUp)
         signInButton=findViewById(R.id.btnSignIn)
