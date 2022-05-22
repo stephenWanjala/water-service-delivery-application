ef2e1e655742068b37bf87c5c1eb9c8bc3d26d64
wanjala stephen <stephenwanjala145@gmail.com>
2022-05-22 04:04:13 +0300

    welcome actv

--- app/src/main/java/com/wtech/waterdeliveryapp/WelcomeActivity.kt
+++ app/src/main/java/com/wtech/waterdeliveryapp/WelcomeActivity.kt
@@ -2,10 +2,26 @@ package com.wtech.waterdeliveryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var signupButton: Button
+    private lateinit var signInButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+
+        signupButton=findViewById(R.id.btnSignUp)
+        signInButton=findViewById(R.id.btnSignIn)
+
+
+
+        signupButton.setOnClickListener {
+
+        }
+        signInButton.setOnClickListener {
+
+        }
     }
 }
